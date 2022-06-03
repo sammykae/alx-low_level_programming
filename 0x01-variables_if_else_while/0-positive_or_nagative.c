3b287bc6bb0681f8ffec9f4aafe36837fe67655f
sammykae <sammykay301@live.com>
2022-06-03 11:49:57 +0100

    positive negative

--- 0x01-variables_if_else_while/0-positive_or_nagative.c
+++ 0x01-variables_if_else_while/0-positive_or_nagative.c
@@ -16,15 +16,15 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n < 0)
 
-printf(n + " is negative\n");
+printf("%d is negative\n", n);
 
 else if (n > 0)
 
-printf(n + " is positive\n");
+printf("%d is positive\n", n);
 
 else
 
-printf(n + " is zero\n");
+printf("%d is zero\n", n);
 
 return (0);
 }
