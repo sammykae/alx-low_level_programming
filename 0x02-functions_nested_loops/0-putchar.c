3aaf3c0171e3be1660fbaaebf1f7debcc47aab10
sammykae <sammykay301@live.com>
2022-06-07 21:59:08 +0100

    loops and functions

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+* main - Print the word "Holberton"
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int i;
+	char p[] = "_putchar";
+
+	for (i = 0; i < 9; i++)
+		{
+		putchar(p[i]);
+		}
+		putchar('\n');
+	return (0);
+}
