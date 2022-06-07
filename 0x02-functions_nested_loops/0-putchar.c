5fed723893ccb5e9ff75a6dbb25798f3040207b6
sammykae <sammykay301@live.com>
2022-06-07 22:25:36 +0100

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,3 @@
-#include "stdio.h"
 #include "main.h"
 /**
 * main - Print the word "_putchar"
@@ -11,7 +10,7 @@ int main(void)
 	int i;
 	char p[] = "_putchar";
 
-	for (i = 0; i < 9; i++)
+	for (i = 0; i < 7; i++)
 		{
 		_putchar(p[i]);
 		}
