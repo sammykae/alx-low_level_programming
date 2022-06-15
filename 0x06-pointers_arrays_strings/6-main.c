aa991088fd92881d66a9eb289ee7fb8ea707ddc2
sammykae <sammykay301@live.com>
2022-06-15 14:44:21 +0100

    madatory tasks

---
+++ 0x06-pointers_arrays_strings/6-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code for 6-cap_string.c
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+  char str[] = "Expect the best. Prepare for the worst. Capitalize on what comes.\nhello world! hello-world 0123456hello world\thello world.hello world\n";
+  char *ptr;
+
+  ptr = cap_string(str);
+  printf("%s", ptr);
+  printf("%s", str);
+  return (0);
+}
