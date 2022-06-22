f5a8ff90d72c41780e37a31d1c8f9fb9d4e28149
sammykae <sammykay301@live.com>
2022-06-22 10:26:17 +0100

    mandatory tasts

---
+++ 0x08-recursion/1-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * main - check the code for 1-print_rev_recirsion.c
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    _print_rev_recursion("\nColton Walker");
+    return (0);
+}
