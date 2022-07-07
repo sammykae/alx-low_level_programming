1afbf770283456d26c77e258b68c6791b6f89694
sammykae <sammykay301@live.com>
2022-07-07 16:00:56 +0100

    update

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,19 +1,43 @@
-#ifndef VARFUNC
-#define VARFUNC
+// #ifndef VARFUNC
+// #define VARFUNC
+// #include <stdarg.h>
+// int sum_them_all(const unsigned int n, ...);
+// void print_numbers(const char *separator, const unsigned int n, ...);
+// void print_strings(const char *separator, const unsigned int n, ...);
+// void print_all(const char * const format, ...);
+// /**
+//  * struct typ - Struct operators
+//  *
+//  * @c: Char
+//  * @tp: The function associated
+//  */
+// typedef struct typ
+// {
+// 	char *c;
+// 	void (*tp)(va_list var);
+// } typ_t;
+// #endif
+
+
+#ifndef _VARIADIC_FUNCTIONS_H_
+#define _VARIADIC_FUNCTIONS_H_
+
 #include <stdarg.h>
+/**
+ * struct print - print type with corresponding print function
+ * @t: print type
+ * @f: print function
+ */
+typedef struct print
+{
+	char *t;
+	void (*f)(va_list);
+} print_t;
+
+int _putchar(char);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-/**
- * struct typ - Struct operators
- *
- * @c: Char
- * @tp: The function associated
- */
-typedef struct typ
-{
-	char *c;
-	void (*tp)(va_list var);
-} typ_t;
-#endif
+
+#endif /* _VARIADIC_FUNCTIONS_H_ */
